ff19b99e9d8299103bf27f9687a68779abbb930e
Kostya Shkryob <kshkryob@gmail.com>
2023-08-19 13:35:32 +0300

    Refactor

--- solar_system_production.py
+++ solar_system_production.py
@@ -1,3 +1,5 @@
+from typing import List
+import pandas
 from models import Address
 from timezonefinder import TimezoneFinder
 from pvlib import pvsystem, iotools, temperature
@@ -7,18 +9,45 @@ from pvlib.modelchain import ModelChain
 
 class SolarSystemProductionService:
     def get_production(self, address: Address):
+        module_parameters = self.create_module_paramenters(address)
         total_modules = sum(surface.max_panels for surface in address.surfaces)
-        local_timezone = self.get_local_timezone(address)
+        inverter_parameters = {'pdc0': address.solar_module.capacity * total_modules}
+        location = self.create_location(address)
+
+        arrays = self.create_arrays(address, module_parameters)
+        weather_datasets = self.create_wather_datasets(address)
+        system = self.create_system(arrays, inverter_parameters)
+        mc = self.create_model_chain(location, system)
+        mc.run_model(weather_datasets)
+        annual_energy = mc.results.ac.sum()
+        annual_energy = annual_energy / 1000  # convert to kWh
+
+        #for
+        #mc.results.ac.between_time()
+
+        return annual_energy
+
+    def create_module_paramenters(self, address: Address) -> dict:
         module_parameters = {
             #  nominal DC power output of a solar module under standard test conditions (STC)
             'pdc0': address.solar_module.capacity,
             # The temperature coefficient of power. Typically -0.002 to -0.005 per degree C. [1/C]
             'gamma_pdc': -0.004,
-
         }
-        inverter_parameters = {'pdc0': address.solar_module.capacity * total_modules}
+        return module_parameters
 
-        temperature_model_parameters = temperature.TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_glass']
+    def create_model_chain(self, location: Location, system: pvsystem.PVSystem) -> ModelChain:
+        return ModelChain(
+            system,
+            location,
+            aoi_model='physical',
+            spectral_model='no_loss',
+            losses_model='pvwatts',
+        )
+
+    def create_location(self, address: Address) -> Location:
+        local_timezone = self.get_local_timezone(address)
+
         location = Location(
             address.latitude,
             address.longitude,
@@ -26,10 +55,30 @@ class SolarSystemProductionService:
             # altitude=altitude,
             tz=local_timezone,
         )
+        return location
 
-        basic_weather = iotools.get_pvgis_tmy(address.latitude, address.longitude)[0]
-        arrays = []
+    def create_wather_datasets(self, address: Address) -> List[pandas.DataFrame]:
         weather_datasets = []
+        basic_weather = iotools.get_pvgis_tmy(address.latitude, address.longitude)[0]
+
+        for surface in address.surfaces:
+            surface_weather = basic_weather.copy()
+            shading_coeff = (100 - surface.shading) / 100
+            # dni - Direct Normal Irradiance
+            surface_weather['dni'] = surface_weather['dni'].apply(lambda x: x * shading_coeff)
+
+            # comment below two lines to get results as in SAM
+            surface_weather['ghi'] = surface_weather['ghi'].apply(
+                lambda x: x * shading_coeff)  # Global Horizontal Irradiance
+            surface_weather['dhi'] = surface_weather['dhi'].apply(
+                lambda x: x * shading_coeff)  # Diffuse Horizontal Irradiance
+            weather_datasets.append(surface_weather)
+
+        return weather_datasets
+
+    def create_arrays(self, address: Address, module_parameters: dict) -> List[pvsystem.Array]:
+        arrays = []
+        temperature_model_parameters = temperature.TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_glass']
 
         for surface in address.surfaces:
             mount = pvsystem.FixedMount(
@@ -42,16 +91,10 @@ class SolarSystemProductionService:
                 temperature_model_parameters=temperature_model_parameters,
                 modules_per_string=surface.max_panels,
             ))
-            surface_weather = basic_weather.copy()
-            shading_coeff = (100 - surface.shading) / 100
-            # dni - Direct Normal Irradiance
-            surface_weather['dni'] = surface_weather['dni'].apply(lambda x: x * shading_coeff)
 
-            # comment below two lines to get results as in SAM
-            surface_weather['ghi'] = surface_weather['ghi'].apply(lambda x: x * shading_coeff)  # Global Horizontal Irradiance
-            surface_weather['dhi'] = surface_weather['dhi'].apply(lambda x: x * shading_coeff)  # Diffuse Horizontal Irradiance
-            weather_datasets.append(surface_weather)
+        return arrays
 
+    def create_system(self, arrays: List[pvsystem.Array], inverter_parameters: dict) -> pvsystem.PVSystem:
         system = pvsystem.PVSystem(
             arrays=arrays,
             inverter_parameters=inverter_parameters,
@@ -70,19 +113,10 @@ class SolarSystemProductionService:
             }
         )
         system.racking_model = None
-        mc = ModelChain(
-            system,
-            location,
-            aoi_model='physical',
-            spectral_model='no_loss',
-            losses_model='pvwatts',
-        )
-        mc.run_model(weather_datasets)
-        annual_energy = mc.results.ac.sum()
-        annual_energy = annual_energy / 1000  # convert to kWh
 
-        return annual_energy
+        return system
 
     def get_local_timezone(self, address: Address) -> str:
         tf = TimezoneFinder()
+
         return tf.timezone_at(lat=address.latitude, lng=address.longitude)
